51988e2857e770b736853c3fc237e6dd0df78702
kaique savioli <kaiquesavioli@gmail.com>
2022-10-27 11:45:53 -0300

    consgeee

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,24 +1,24 @@
+//cria uma variavel para manipular o arquivo xml
 xmlhttp = new XMLHttpRequest();
-
-xmlhttp.open("GET","xml/conteudo.xml",false);
-
+// ler um determinado arquivo no XML nodiretório do projeto
+xmlhttp.open("GET","xml/conteudo.xml", false);
+//envia o arquivo aberto para leitura
 xmlhttp.send();
-
+// envia uma resposta do tipo XML
 xmlDoc = xmlhttp.responseXML;
 
 x = xmlDoc.getElementsByTagName("postagem");
 
-function funcaoConteudo()
-{
-    for(i = 0;i <= 2; i++)
-    {
 
+function funcaoConteudo(){
+    for(i=0; i<=2; i++){
     document.write(
-        "<tr>" + 
-            "<td>" + x[i].getAttribute("codigo") + "</td>" +
-            "<td>B</td>" +
-            "<td>C</td>" +
-            "<td>D</td>" +
-        "</tr>");
+        "<tr>"  +
+        "<td>" + x[i].getAttribute("codigo") + "</td>" +
+        "<td><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue+"' width='150'></td>" +
+        "<td>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue+"</td>" +
+        "<td>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue+"</td>" +
+       "</tr>"
+       );
     }
 }
